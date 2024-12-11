00fd14ccdca2c94b71421bae68dd96221ea62ceb
green <41323182+greeeen-dev@users.noreply.github.com>
2024-12-11 22:25:20 +0100

    pull repo instead

--- scripts/bump.py
+++ scripts/bump.py
@@ -1,8 +1,10 @@
 import json
-import sys
 
-version = sys.argv[1]
-release = int(sys.argv[2])
+with open('unifier/plugins/system.json', 'r') as file:
+    unifier_data = json.load(file)
+
+release = unifier_data['release']
+version = unifier_data['version']
 
 with open('update.json', 'r') as file:
     data = json.load(file)
